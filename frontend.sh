e0add1e6083893ac16e93cc2718973ab86e1b858
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-04 01:38:25 -0800

    'common-code

--- frontend.sh
+++ frontend.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
-#source ./common.sh 
-#check_root
-
-USERID=$(id -u)
-LOG_FOLDER="/var/log/roboshop"
-LOG_FILE="$LOG_FOLDER/$0.log"
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[34m"
-SCRIPT_DIR=$PWD
-
-
-if [ $USERID -ne 0 ]; then
-     echo -e "$R please run this script using root user" | tee -a $LOG_FILE
-     exit 1
-fi
-
-mkdir -p $LOG_FOLDER
-
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo -e " $2... $R FAILURE" | tee -a $LOG_FILE
-    else
-        echo -e "$2... $G SUCCESS" | tee -a $LOG_FILE
-    fi
-}
+source ./common.sh 
+app_name=frontend
+app_dir=/usr/share/nginx/html/
+check_root
 
 dnf module disable nginx -y       &>>$LOG_FILE
 dnf module enable nginx:1.24 -y    &>>$LOG_FILE
@@ -52,9 +29,9 @@ rm -rf /etc/nginx/nginx.conf
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf      
 VALIDATE  $? "copying conf file"
 
-systemctl restart nginx       &>>$LOG_FILE
-VALIDATE  $? "restart  nginx"
+#systemctl restart nginx       &>>$LOG_FILE
+#VALIDATE  $? "restart  nginx"
 
 
-#app_restart
-#print_total_time
+app_restart
+print_total_time
