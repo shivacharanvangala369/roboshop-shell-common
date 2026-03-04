dd7e075d5c11e4274f4050559be5a10842f730a7
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-03 23:24:15 -0800

    common-code

---
+++ catalogue.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=catalogue
+
+check_root
+app_setup
+nodejs_step
+systemd_setup
+
+### Loading data into MongoDB####
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo  
+VALIDATE  $? "copying mongorepo"
+
+
+
+dnf install mongodb-mongosh -y    &>>$LOG_FILE
+VALIDATE  $? "installing mogo-client"
+
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexO1oOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e " $(date "+%Y-%m-%d %H:%M:%S") | Products already loaded ... $Y SKIPPING $N"
+fi
+
+
+app_restart
--- common.sh
+++ common.sh
@@ -7,6 +7,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[34m"
+SCRIPT_DIR=PWD
+MONGODB_HOST=mongodb.devcops.online
 START_TIME=$(date +%s)
 
 mkdir -p $LOG_FOLDER
@@ -28,6 +30,65 @@ VALIDATE(){
     else
         echo -e "$(date "+%Y-%m-%d %H:%M:%S") |$2... $G SUCCESS" | tee -a $LOG_FILE
     fi
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y  &>>$LOG_FILE
+    VALIDATE  $? "module disable nodejs"
+
+    dnf module enable nodejs:20 -y   &>>$LOG_FILE
+    VALIDATE  $? "module enable nodejs"
+
+
+    dnf install nodejs -y    &>>$LOG_FILE
+    VALIDATE  $? "install nodejs"
+
+    cd /app           &>>$LOG_FILE
+    npm install      &>>$LOG_FILE
+    VALIDATE  $? "installing dependencies"
+ 
+}
+
+app_setup(){
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin  --comment "roboshop system user" roboshop &>>$LOG_FILE
+        VALIDATE $? "Adding roboshop as system user"
+    else
+        echo -e "Roboshop user alraedy exist...$Y SKIPPLNG $N"
+    fi
+
+    mkdir -p /app  &>>$LOG_FILE
+    VALIDATE  $? "creating app dir in / "
+
+
+    curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip   &>>$LOG_FILE
+    VALIDATE  $? "downloading $app_name source code"
+
+    cd /app  &>>$LOG_FILE
+
+    rm -rf /app/*   &>>$LOG_FILE
+    VALIDATE  $? "removeing app dir in /"
+
+    unzip /tmp/catalogue.zip    &>>$LOG_FILE
+    VALIDATE  $? "unzipping $app_name source code"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service 
+    VALIDATE  $? "copying $app_name service"
+
+    systemctl daemon-reload    &>>$LOG_FILE
+    VALIDATE  $? "daemon-reload"
+
+    systemctl enable catalogue    &>>$LOG_FILE
+    systemctl start catalogue     &>>$LOG_FILE
+    VALIDATE  $? "enable and starting $app_namealouge services"
+}
+
+app_restart(){
+    systemctl restart $app_name     &>>$LOG_FILE
+    VALIDATE  $? "restarting $app_name services"
 }
 
 print_total_time(){
